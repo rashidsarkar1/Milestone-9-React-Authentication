59512bd8b121ce92dd239f6b9cefb9af130a8ed2
Rashid Sarker <rashidrock1234@gmail.com>
2024-04-04 23:45:29 +0600

    Update firebaseConfig .js

--- name-of-your-project/src/FireBase/firebaseConfig .js
+++ name-of-your-project/src/FireBase/firebaseConfig .js
@@ -5,12 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD7c-F-RkuiEHgQglZQ7l7aLfWiETlOX6w",
-  authDomain: "asssinment-9-1srt-attem.firebaseapp.com",
-  projectId: "asssinment-9-1srt-attem",
-  storageBucket: "asssinment-9-1srt-attem.appspot.com",
-  messagingSenderId: "154786916259",
-  appId: "1:154786916259:web:41042e1979a0503dd71c16",
+
 };
 
 // Initialize Firebase
