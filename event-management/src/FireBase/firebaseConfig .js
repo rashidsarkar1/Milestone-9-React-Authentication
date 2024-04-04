7bada1ae08fef60ac27fb81bacf584e9a74bb548
Rashid Sarker <rashidrock1234@gmail.com>
2024-04-04 23:44:00 +0600

    Update firebaseConfig .js removed firebase code

--- event-management/src/FireBase/firebaseConfig .js
+++ event-management/src/FireBase/firebaseConfig .js
@@ -4,14 +4,9 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyD7c-F-RkuiEHgQglZQ7l7aLfWiETlOX6w",
-  authDomain: "asssinment-9-1srt-attem.firebaseapp.com",
-  projectId: "asssinment-9-1srt-attem",
-  storageBucket: "asssinment-9-1srt-attem.appspot.com",
-  messagingSenderId: "154786916259",
-  appId: "1:154786916259:web:41042e1979a0503dd71c16",
-};
+// const firebaseConfig = {
+// ,
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
